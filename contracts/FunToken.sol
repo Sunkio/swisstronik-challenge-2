cb2d004a838a342d0fd6889fdafb354bdf6155e3
Tanja Schmidt <tagretha@gmail.com>
2023-09-16 14:54:43 +0200

    add FunToken contract

---
+++ contracts/FunToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FunToken is ERC20 {
+    constructor()ERC20("FunToken","FUTO"){
+        _mint(msg.sender, 100 * 10 ** decimals());
+    } 
+
+    function mint100Tokens() public {
+        _mint(msg.sender, 100 * 10 ** decimals());
+    }
+
+    function burn100Tokens() public {
+        _burn(msg.sender,100 * 10 ** decimals());
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
+      accounts: [PRIVATE_KEY], //Your private key starting with "0x"
+      //Make sure you have enough funds in this wallet to deploy the smart contract
+    },
+  },
 };
